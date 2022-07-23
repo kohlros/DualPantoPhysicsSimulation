b53f30ce9e5bdb0ca1fa8f95b9285b84830e8ca3
kohlros <kohlros@uni-potsdam.de>
2022-07-23 14:39:05 +0200

    change charge with handle

--- Assets/PlayerChargeScript.cs
+++ Assets/PlayerChargeScript.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DualPantoFramework;
 
 public class PlayerChargeScript : MonoBehaviour
 {
     public static float meCharge = 1f;
+    PantoHandle upperHandle;
+    float zeroAngle; 
+    float currentAngle;
+    float angleDiff;
 
+    void Start()
+    {
+        upperHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
+        zeroAngle = upperHandle.GetRotation();
+    }
 
     void FixedUpdate(){
-        if(meCharge <= 4f){
-            meCharge += 0.1f;
-        }
-        Debug.Log("meCharge:" + meCharge);
+        // if(meCharge <= 4f){
+        //     meCharge += 0.1f;
+        // }
+        // Debug.Log("meCharge:" + meCharge);
+        currentAngle = upperHandle.GetRotation();
+        angleDiff = zeroAngle - currentAngle; 
+        meCharge = Mathf.Abs((angleDiff/360)*10);
     }
 
 }
